c57e2e60c98624d58c030c6b8241ddb550d28ed5
Lauviah Fang <lauviah0622@gmail.com>
2020-12-11 15:36:52 +0800

    ci(lint): add eslint and commit check

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.jsx';
 
+test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/123/i);
+    expect(linkElement).toBeInTheDocument();
+  });
